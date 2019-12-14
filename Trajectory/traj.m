64b09cdfaac4ed63b76c603f99c80d8aba37a64e
Sonny Li <sonnyli@airbears2-10-142-173-127.airbears2.1918.berkeley.edu>
2019-12-14 00:49:44 -0800

    Bay Area Trajectory

--- Trajectory/traj.m
+++ Trajectory/traj.m
@@ -3,44 +3,56 @@
 close all; clc; clear all;
 
 % 32187 is 20mi
-r.x = 32187;
-r.y = 32187;
-r.z = 32187;
-
-theta = linspace(0,2*pi,360);
-x = r.x*cos(theta);
-y = r.y*sin(theta);
-z = r.z*sin(theta);
-circlewaypoints = [x',y',z'];
-plot3(x,y,z)
-grid on
-axis square
-
-% WP =...
-%     [0 0 0;
-%     25 0 25;
-%     40 0 50;
-%     60 0 50;
-%     80 0 25;
-%     100 0 0;
-%     80 30 25;
-%     60 40 50;
-%     40 60 50;
-%     20 80 50;
-%      0 120 0; 
-%     -5 100 25;
-%     -5 80 30;
-%    -10 60 50;
-%    -10 40 50;
-%    -10 25 25;
-%      0  0 0];
-% temp = cat(1,0,cumsum(sqrt(sum(diff(WP,[],1).^2,2))));
-% interpWP = interp1(temp, WP, unique([temp(:)' linspace(0,temp(end),200)]),'cubic');
+% r.x = 32187;
+% r.y = 32187;
+% r.z = 32187;
 % 
-% figure, hold on
-% plot3(WP(:,1),WP(:,2),WP(:,3),'.b-')
-% plot3(interpWP(:,1),interpWP(:,2),interpWP(:,3),'.r-')
-% 
+% theta = linspace(0,2*pi,360);
+% x = r.x*cos(theta);
+% y = r.y*sin(theta);
+% z = r.z*sin(theta);
+% circlewaypoints = [x',y',z'];
+% plot3(x,y,z)
 % grid on
-% title('Quadcopter Trajectoy')
-% axis image, view(3), legend({'Original','Interp. Spline'})
+% axis square
+
+for i = 0:length(exportWP)-1
+    WP(i+1,:) = exportWP(length(exportWP)-i).Position
+end
+
+for i = 1:length(exportWP)
+    distWP(i) = sqrt((WP(i+1,1)-WP(i,1))^2+(WP(i+1,2)-WP(i,2))^2);
+end
+
+WP(12,:) = WP(1,:);
+WP(:,3) = zeros(12,1);
+WP(:,3) = ...
+    [0;
+    100;
+    100;
+    0;
+    100;
+    100;
+    0;
+    100;
+    100;
+    100;
+    100;
+    0];
+WP = 5000/700*WP;
+
+temp = cat(1,0,cumsum(sqrt(sum(diff(WP,[],1).^2,2))));
+interpWP = interp1(temp, WP, unique([temp(:)' linspace(0,temp(end),1430)]),'PCHIP');
+
+
+save WP
+% 
+figure, hold on
+% open('BayAreaMap.fig')
+plot3(WP(:,1),WP(:,2),WP(:,3),'.b-')
+plot3(interpWP(:,1),interpWP(:,2),interpWP(:,3),'.r-')
+
+
+grid on
+title('Quadcopter Trajectoy')
+axis image, view(3), legend({'Original','Interp. Spline'})
